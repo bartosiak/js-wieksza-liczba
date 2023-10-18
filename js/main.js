ba7cde6da7ba6305a736a05061783507266dc512
Bartosiak <bartosiak.contact@gmail.com>
2023-10-18 13:27:22 +0200

    stworzenie pliku

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let num1 = 2;
+let num2 = 5;
+
+function biggerNumber(a, b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+console.log(biggerNumber(num1, num2));
